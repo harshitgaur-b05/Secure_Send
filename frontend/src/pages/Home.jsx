6771331df88231f647d9f295716190e18f9213cf
harshitgaur077 <harshitgaur737@gmail.com>
2024-09-26 15:18:55 +0530

    4 commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,11 @@ const HomePage = () => {
             <h1 className="text-4xl font-bold mb-4">Welcome to Our App</h1>
             <p className="mb-6">Please sign in or sign up to continue.</p>
             <div className="space-x-4">
-                <Link to="/signin">
+                {/* <Link to="/signin">
                     <button className="bg-blue-500 text-white px-4 py-2 rounded">
                         Sign In
                     </button>
-                </Link>
+                </Link> */}
                 <Link to="/signup">
                     <button className="bg-green-500 text-white px-4 py-2 rounded">
                         Sign Up
